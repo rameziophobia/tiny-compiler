ea33d6c3ca79b08420ad1f14393f2d84242dba29
Ramez <rameznoshy@gmail.com>
2020-03-06 14:55:21 +0200

    initial commit

---
+++ Controller/Scanner.cs
@@ -0,0 +1,35 @@
+﻿using TinyCompiler.Model;
+
+namespace TinyCompiler.Controller
+{
+    class Scanner
+    {
+        
+        public Token getToken()
+        {
+            string lexeme = "";
+            TokenType type = TokenType.EndOfFile;
+            Token token = new Token();
+            //todo swich case/loop, set in it the type and lexeme
+
+            token.Lexeme = lexeme;
+            token.Type = type;
+            if(token.Type == TokenType.Id)
+            {
+                setTypeIfReserved(token);
+            }
+            return token;
+        }
+
+        private void setTypeIfReserved(Token token)
+        {
+            foreach(string word in Token.RESERVED_WORDS.Keys)
+            {
+                if (token.Lexeme.Equals(word))
+                {
+                    token.Type = Token.RESERVED_WORDS[word];
+                }
+            }
+        }
+    }
+}
---
+++ Model/Token.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum TokenType
+{
+    Id,
+    EndOfFile,
+    Error,
+    Num,
+    If, 
+    Then, 
+    Else, 
+    End, 
+    Repeat,
+    Until, 
+    Read,
+    Write,
+    Equal
+    //todo +special symbols =, <, * etc
+}
+
+enum State
+{
+    Start,
+    Comment //todo lesa 7aba stateskaman
+}
+
+namespace TinyCompiler.Model
+{
+    class Token
+    {
+        public readonly static Dictionary<string, TokenType> RESERVED_WORDS = 
+            new Dictionary<string, TokenType>{
+                { "if" , TokenType.If}, 
+                { "then" , TokenType.Then}, 
+                { "else" , TokenType.Else}, 
+                { "end" , TokenType.End}, 
+                { "repeat" , TokenType.Repeat}, 
+                { "until" , TokenType.Until}, 
+                { "read" , TokenType.Read}, 
+                { "write" , TokenType.Write}
+            };
+
+        public readonly static Dictionary<string, TokenType> SPECIAL_SYMBOLS = 
+            new Dictionary<string, TokenType>{
+                { "=", TokenType.Equal },
+                { "=", TokenType.Equal },
+                { "=", TokenType.Equal },
+                { "=", TokenType.Equal },
+                { "=", TokenType.Equal },
+                { "=", TokenType.Equal } //todo complete this
+            };
+
+        public TokenType Type { get; set; }
+        public int Value { get; set; }
+        public string Lexeme { get; set; }
+    }
+}
