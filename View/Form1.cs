4fbe872beb086c94de487fe5370e2e0626f659ee
Aboody <starfire19990@gmail.com>
2020-03-10 02:15:37 +0200

    Gui first iteration

--- View/Form1.cs
+++ View/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,38 @@ namespace TinyCompiler
         public Form1()
         {
             InitializeComponent();
+        }
+        private void OpenFile_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog.OpenFile() != null)
+                {
+                    string fileText = File.ReadAllText(openFileDialog.FileName);
+                    CodeText.Text = fileText;
+                }
+            }
+        }
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Edit_button_Click(object sender, EventArgs e)
+        {
+            CodeText.ReadOnly = false;
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            //TODO: copy richTextBox1.Text into a temp file
+            CodeText.ReadOnly = true;
         }
     }
 }
