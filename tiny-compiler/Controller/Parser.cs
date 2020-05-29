a5a7480558004936632c977871572d15dc2eade9
Ramez <rameznoshy@gmail.com>
2020-05-29 15:06:00 +0200

    hotfix: match else

--- tiny-compiler/Controller/Parser.cs
+++ tiny-compiler/Controller/Parser.cs
@@ -1,31 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using TinyCompiler.Model;
 using TinyCompiler.Model.Errors;
 
 namespace TinyCompiler.Controller
 {
-    // Task I
-    // todo custom exception
-    // todo match()
-    // todo display errors on the GUI
-
-    // Task II
-    // todo change rules to EBNF and implement their methods
-    // todo implement methods: program stmt_sequence, statement, if_stmt, read_stmt, write_stmt, assign_stmt
-
-    // Task III
-    // todo change rules to EBNF and implement their methods
-    // *note law 3ayzin t2asemo el methods ma3 ba3d differently -> sure
-    // todo repeat_stmt, exp, comparison_op, simple_exp, add_op, term, mul_op, factor
-
-    // Task IV
-    // todo display the tree on the screen and architect the tree data structure
-    // todo implement methods to add to that Tree structure
-
-    // Task V
-    // todo implement the general Parser class structure and helping w/ task 4
-    // todo architect the system we ay 7aga fadla 3mtn
-    // ana wa5ed el task da xD
 
     class Parser
     {
@@ -40,7 +18,7 @@ namespace TinyCompiler.Controller
             tokens.Add(endOfFile);
         }
 
-        public TreeNode parse() //if incorrect location to make the exception handling, TODO make safe parse
+        public TreeNode parse()
         {
             try
             {
@@ -123,6 +101,8 @@ namespace TinyCompiler.Controller
             currentTokenIndex++;
             if (tokens[currentTokenIndex].Type == TokenType.Else)
             {
+                match(TokenType.Else);
+                currentTokenIndex++;
                 treeNode.Children.Add(getStmtSequence());
                 currentTokenIndex++;
             }
